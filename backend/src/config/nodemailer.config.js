28445d55771e11231694d9bbde82c15bc2866487
Mansuri786-Jiyan <mansuriziyan8888@gmail.com>
2026-03-31 17:32:19 +0530

    feat: configure nodemailer transporter with SMTP environment variables

--- backend/src/config/nodemailer.config.js
+++ backend/src/config/nodemailer.config.js
@@ -2,11 +2,11 @@ import nodemailer from "nodemailer";
 import { Env } from "./env.config.js";
 
 export const transporter = nodemailer.createTransport({
-    host: Env.SMTP_HOST,
-    port: Number(Env.SMTP_PORT),
-    secure: Number(Env.SMTP_PORT) === 465,
-    auth: {
-        user: Env.MAIL_USER,
-        pass: Env.MAIL_PASS,
-    },
+  host: Env.SMTP_HOST,          
+  port: Number(Env.SMTP_PORT), 
+  secure: false,               
+  auth: {
+    user: Env.MAIL_USER,
+    pass: Env.MAIL_PASS,
+  },
 });
